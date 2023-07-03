81181590d003dc24732228dfaba022f9a34da715
Pradee67 <109140915+Pradee67@users.noreply.github.com>
2023-07-03 21:47:59 +0530

    added paths

--- scripts/ApplicationStart.ps1
+++ scripts/ApplicationStart.ps1
@@ -4,7 +4,7 @@
 Get-Process -Name "ConsolePipeline" -ErrorAction SilentlyContinue | Stop-Process -Force
 
 # Specify the path to your console application executable
-$consoleAppPath = ""C:\PipelineDeployment\ApiPipelineDeploy\PipelineTesting"
+$consoleAppPath = "C:\MyConsoleapp\ConsoleApplication\ConsolePipeline"
 # Clean the application directory
 Remove-Item -Path $consoleAppPath\* -Recurse -Force
 dotnet publish -c Release -o $consoleAppPath
